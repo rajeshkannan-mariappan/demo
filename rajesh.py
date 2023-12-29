b3c89c46a2754eb71118c68060465ce6e5146e31
rajeshkannan-mariappan <154425726+rajeshkannan-mariappan@users.noreply.github.com>
2023-12-29 23:24:19 +0530

    Update rajesh.py

--- rajesh.py
+++ rajesh.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.write('puppy')
 st.header('puppy')
 st.image('https://i.pinimg.com/564x/36/b4/ef/36b4ef433ac9c2cf9d34e54aa3a38340.jpg', caption = 'puppy already sold')
-audio_file = open('Mudhal Nee Mudivum Nee.mp3', 'rb').read()
+audio_file = open('https://drive.google.com/file/d/112QHrmuhyIKrpsPE_fGl_ZR4xx7OLUA0/view?usp=drive_link', 'rb').read()
 st.audio(audio_file, format='audio/mp3')
