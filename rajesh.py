b478c3b476324211b3eb552cd0c46ebbcc282766
rajeshkannan-mariappan <154425726+rajeshkannan-mariappan@users.noreply.github.com>
2023-12-29 23:37:34 +0530

    Update rajesh.py

--- rajesh.py
+++ rajesh.py
@@ -2,5 +2,13 @@ import streamlit as st
 st.write('puppy')
 st.header('puppy')
 st.image('https://i.pinimg.com/564x/36/b4/ef/36b4ef433ac9c2cf9d34e54aa3a38340.jpg', caption = 'puppy already sold')
-audio_file = open('https://drive.google.com/file/d/112QHrmuhyIKrpsPE_fGl_ZR4xx7OLUA0/view?usp=drive_link', 'rb').read()
+import gdown
+file_id = '112QHrmuhyIKrpsPE_fGl_ZR4xx7OLUA0'
+url = f'https://drive.google.com/uc?id={file_id}'
+
+output = './myaudio.mp3'  # Replace with your desired local path and filename
+
+gdown.download(url, output, quiet=False)
+
+audio_file = open('./myaudio.mp3', 'rb').read()
 st.audio(audio_file, format='audio/mp3')
